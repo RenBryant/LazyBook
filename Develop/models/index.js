87f6183b1232c41b8cdae8a018795585f08931c3
RenBryant <renbee2bee@gmail.com>
2022-07-22 07:21:29 -0400

    added Models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,6 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'product_id',
+  through: {model: ProductTag, unique: false},
+});
+
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'tagt_id',
+  through: {model: ProductTag, unique: false},
+});
+
 // Products belongsTo Category
 
 // Categories have many Products
