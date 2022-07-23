105a1538f1319add522b70d4357ed4d03ed8678d
RenBryant <renbee2bee@gmail.com>
2022-07-22 23:35:10 -0400

    finish category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,33 +17,43 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  try {
+    const infoCategory = await Category.findOne({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if (!infoCategory) {
+      res.status(400).json({ message: 'Incorrect Credentials!' });
+      return;
+    }
+    res.status(200).json(infoCategory);
+  } catch (err) {res.status(500).json(err)}
+  });
 
 router.post('/', async (req, res) => {
   // create a new category
   const body = req.body;
 
   try {
-    const newPost = await Post.create({ ...body, userId: req.session.userId });
-    res.json(newPost);
-  } catch (err) {
-    res.status(500).json(err);
-}});
+    const infoCategory = await Category.create({category_name: req.body.category_name });
+    res.status(200).json(infoCategory);
+  } catch (err) {res.status(500).json(err)}
+});
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const [affectedRows] = await Post.update(req.body, {
+    const updateCategory = await Category.update({
+      id: req.body.id,
+      category_name: req.body.category_name,
+    },
+    {
       where: {
         id: req.params.id,
       },
     });
-
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
+    res.json(updateCategory)
   } catch (err) {
     res.status(500).json(err);
   }
@@ -52,17 +62,12 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const [affectedRows] = Post.destroy({
+    const deleteCategory = await Category.destroy({
       where: {
         id: req.params.id,
       },
     });
-
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
+    res.json(deleteCategory)
   } catch (err) {
     res.status(500).json(err);
   }
