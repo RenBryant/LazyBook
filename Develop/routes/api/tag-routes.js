983d215a4727cd19fb34ac0f84a9c63fc9b1f0ab
RenBryant <renbee2bee@gmail.com>
2022-07-22 23:52:55 -0400

    finished API routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,69 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const infoTag = await Tag.findAll({
+      include: [{
+        model: Product,
+      }],
+    })
+    res.status(200).json(infoTag);
+  } catch (err) {res.status(500).json(err)}
 });
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const infoTag = await Tag.findOne(req.params.id, {
+      include: [{model: Product}],
+    })
+    if (!infoTag) {
+      res.status(400).json({ message: 'Incorrect Credentials!' });
+      return;
+    }
+    res.status(200).json(infoCategory);
+  } catch (err) {res.status(500).json(err)}
 });
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const infoTag = await Tag.create({tag_name: req.body.tag_name });
+    res.status(200).json(infoTag);
+  } catch (err) {res.status(500).json(err)}
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update({
+      id: req.body.id,
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(updateTag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(deleteTag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
